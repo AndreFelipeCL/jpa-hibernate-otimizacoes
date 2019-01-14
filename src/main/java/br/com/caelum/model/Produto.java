35671b0157f2421c71187f75f599e1872d91a9ca
André Felipe Camargo Leite <andrefelipecl@outlook.com>
2019-01-14 17:23:06 -0200

    Produto com relacionamento ManyToMany com Categoria

--- src/main/java/br/com/caelum/model/Produto.java
+++ src/main/java/br/com/caelum/model/Produto.java
@@ -9,8 +9,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.validation.Valid;
 import javax.validation.constraints.Min;
 
@@ -34,7 +34,7 @@ public class Produto {
 	@Min(20)
 	private double preco;
 	
-	@OneToMany
+	@ManyToMany
 	private List<Categoria> categorias = new ArrayList<>();
 	
 	@Valid
